04a74589001ec76a336209653ff866cee42481ac
vinitbhisesh <vinit.bhise@shauryatechnosoft.com>
2019-10-19 19:19:44 +0530

    changes

---
+++ src/app/complaint-chat/complaint-chat.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ComplaintChatComponent } from './complaint-chat.component';
+
+describe('ComplaintChatComponent', () => {
+  let component: ComplaintChatComponent;
+  let fixture: ComponentFixture<ComplaintChatComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ComplaintChatComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ComplaintChatComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/complaint-chat/complaint-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-complaint-chat',
+  templateUrl: './complaint-chat.component.html',
+  styleUrls: ['./complaint-chat.component.css']
+})
+export class ComplaintChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
